fc71f60416f2ff20ff8332cee19614e8b4d32a5a
Long Yang <jasonyl0806@yahoo.com>
2014-02-24 16:20:15 -0500

    HW3: 2nd commit (1). Finish the interface View, including all the comments.

--- src/com/longyang/abalone/api/View.java
+++ src/com/longyang/abalone/api/View.java
@@ -16,7 +16,28 @@ import com.longyang.abalone.impl.AbaloneState;
  *
  */
 public interface View {
-	
+	/**
+	 * Set the presenter for viewer, and the viewer can call certain methods on presenter, e.g.
+	 * (1). When a jump is placed ({@link AbalonePresenter#pieceJumped()})
+	 * (2). When all the jumps are placed, player want to finish his round 
+	 * ({@link AbalonePresenter#finishedJumpingPieces()})
+	 * 
+	 * One round for a player is viewed by viewer as following ways:
+	 * (1). The viewer calls ({@link AbalonePresenter#pieceJumped()}) to place pieces 
+	 * jump several times (of cause, it can also use this to cancel previous jumps,
+	 * so this should be checked in the source of {@link AbalonePresenter#pieceJumped()}).
+	 * (2). The viewer finishes all of his/her piece jumps and calls 
+	 * ({@link AbalonePresenter#finishedJumpingPieces()}), and this method will compute which
+	 * opponent's pieces will be pushed.
+	 * 
+	 * One round for a player is viewed by presenter as following ways:
+	 * (1). The presenter calls ({@link #nextPieceJump()}) to send the Board back to viewer,
+	 * who can use this to update the graphics.
+	 * (2). The presenter calls ({@link #finishMoves()}) to sent the Board back to viewer,
+	 * who can use this to update the graphics.
+	 * 
+	 * @param abalonePresenter input abalonePresenter.
+	 */
 	public void setPresenter(AbalonePresenter abalonePresenter);
 	
 	/**
@@ -25,7 +46,22 @@ public interface View {
 	 */
 	public void setPlayerState(List<ImmutableList<String>> board);
 	
-	public void nextPieceJump(List<ImmutableList<String>> board);
+	/**
+	 * Ask the player to choose the next piece to jump.
+	 * We pass the update {@code board} from presenter, and the {@code previousJumps} which is
+	 * used to highlight the previous jumps made by this player and it helps him to cancel 
+	 * previous jumps or make the further jumps (by calling {@link AbalonePresenter#pieceJumped()}).
+	 * 
+	 * @param board input updated board from presenter
+	 * @param previousJumps input previous jumps made by this player.
+	 */
+	public void nextPieceJump(List<ImmutableList<String>> board, List<Integer> previousJumps);
 	
+	/**
+	 * After player makes all the jumps, he/she will call 
+	 * {@link AbalonePresenter#finishedJumpingPieces()} to finish the jumps, and it will call this 
+	 * method to update graphics with the updated board.
+	 * @param board input updated board.
+	 */
 	public void finishMoves(List<ImmutableList<String>> board);
 }
